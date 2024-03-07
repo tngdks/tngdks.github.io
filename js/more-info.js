e9c023b4b464ba618d0f21a6cae997a8032c4e79
Devendra Kumar Sharma <mrdsharmar363@gmail.com>
2024-03-07 23:11:36 +0530

    Add files via upload

---
+++ js/more-info.js
@@ -0,0 +1,168 @@
+let info = document.getElementById('info-container');
+let title = document.getElementById('page-title');
+let heroInfo = JSON.parse(localStorage.getItem("heroInfo"));
+title.innerHTML = heroInfo.name + " | SH";
+
+window.addEventListener("load", function () {
+     let favouritesCharacterIDs = localStorage.getItem("favouritesCharacterIDs");
+     if (favouritesCharacterIDs == null) {
+          favouritesCharacterIDs = new Map();
+     } else if (favouritesCharacterIDs != null) {
+          favouritesCharacterIDs = new Map(JSON.parse(localStorage.getItem("favouritesCharacterIDs")));
+     }
+     info.innerHTML =
+          `
+               <div class="flex-row hero-name">${heroInfo.name}</div>
+               <div class="flex-row hero-img-and-more-info">
+                    <img id="portraitImage" class="hero-img" src="${heroInfo.portraitImage}" alt="">
+                    <img style="display:none;" id="landscapeImage" src="${heroInfo.landscapeImage}" alt="">
+                    <div class="flex-col more-info">
+                         <div class="flex-row id">
+                              <b>ID:</b><span>${heroInfo.id}</span>
+                         </div>
+                         <div class="flex-row comics">
+                              <b>Comics:</b><span>${heroInfo.comics}</span>
+                         </div>
+                         <div class="flex-row series">
+                              <b>Series:</b><span>${heroInfo.series}</span>
+                         </div>
+                         <div class="flex-row stories">
+                              <b>Stories:</b><span>${heroInfo.stories}</span>
+                         </div>
+                    </div>
+               </div>
+               <div class="flex-col hero-discription">
+                    <b>Discription:</b>
+                    <p>${heroInfo.description != "" ? heroInfo.description : "No Description Available"}</p>
+               </div>
+               <div style="display:none;">
+                    <span>${heroInfo.name}</span>
+                    <span>${heroInfo.portraitImage}</span>
+                    <span>${heroInfo.landscapeImage}</span>
+                    <span>${heroInfo.id}</span>
+                    <span>${heroInfo.comics}</span>
+                    <span>${heroInfo.series}</span>
+                    <span>${heroInfo.stories}</span>
+                    <span>${heroInfo.squareImage}</span>
+                    <span>${heroInfo.description}</span>
+               </div>
+               <button class="btn add-to-fav-btn">${favouritesCharacterIDs.has(`${heroInfo.id}`) ? "<i class=\"fa-solid fa-heart-circle-minus\"></i> &nbsp; Remove from Favourites" :"<i class=\"fa-solid fa-heart fav-icon\"></i> &nbsp; Add to Favourites</button>"}
+          `
+     addEvent();
+})
+window.addEventListener('resize', function () {
+     let portraitImage = document.getElementById('portraitImage');
+     let landscapeImage = document.getElementById('landscapeImage');
+
+     if (document.body.clientWidth < 678) {
+          portraitImage.style.display = "none";
+          landscapeImage.style.display = "block";
+     } else {
+          landscapeImage.style.display = "none";
+          portraitImage.style.display = "block";
+     }
+})
+function addEvent() {
+     let favouriteButton = document.querySelector('.add-to-fav-btn');
+     favouriteButton.addEventListener("click", addToFavourites);
+}
+
+
+function addToFavourites() {
+     if (this.innerHTML == '<i class="fa-solid fa-heart fav-icon"></i> &nbsp; Add to Favourites') {
+          let heroInfo = {
+               name: this.parentElement.children[3].children[0].innerHTML,
+               description: this.parentElement.children[3].children[8].innerHTML,
+               comics: this.parentElement.children[3].children[4].innerHTML,
+               series: this.parentElement.children[3].children[5].innerHTML,
+               stories: this.parentElement.children[3].children[6].innerHTML,
+               portraitImage: this.parentElement.children[3].children[1].innerHTML,
+               id: this.parentElement.children[3].children[3].innerHTML,
+               landscapeImage: this.parentElement.children[3].children[2].innerHTML,
+               squareImage: this.parentElement.children[3].children[7].innerHTML
+          }
+          let favouritesArray = localStorage.getItem("favouriteCharacters");
+          if (favouritesArray == null) {
+               favouritesArray = [];
+          } else {
+               favouritesArray = JSON.parse(localStorage.getItem("favouriteCharacters"));
+          }
+          let favouritesCharacterIDs = localStorage.getItem("favouritesCharacterIDs");
+          if (favouritesCharacterIDs == null) {
+               favouritesCharacterIDs = new Map();
+          } else {
+               favouritesCharacterIDs = new Map(JSON.parse(localStorage.getItem("favouritesCharacterIDs")));
+          }
+          favouritesCharacterIDs.set(heroInfo.id, true);
+          favouritesArray.push(heroInfo);
+          localStorage.setItem("favouritesCharacterIDs", JSON.stringify([...favouritesCharacterIDs]));
+          localStorage.setItem("favouriteCharacters", JSON.stringify(favouritesArray));
+          this.innerHTML = '<i class="fa-solid fa-heart-circle-minus"></i> &nbsp; Remove from Favourites';
+          document.querySelector(".fav-toast").setAttribute("data-visiblity","show");
+          setTimeout(function(){
+               document.querySelector(".fav-toast").setAttribute("data-visiblity","hide");
+          },1000);
+     }
+     else{
+          let idOfCharacterToBeRemoveFromFavourites = this.parentElement.children[3].children[3].innerHTML;
+          let favouritesArray = JSON.parse(localStorage.getItem("favouriteCharacters"));
+          let favouritesCharacterIDs = new Map(JSON.parse(localStorage.getItem("favouritesCharacterIDs")));
+          let newFavouritesArray = [];
+          favouritesCharacterIDs.delete(`${idOfCharacterToBeRemoveFromFavourites}`);
+          favouritesArray.forEach((favourite) => {
+               if(idOfCharacterToBeRemoveFromFavourites != favourite.id){
+                    newFavouritesArray.push(favourite);
+               }
+          });
+          localStorage.setItem("favouriteCharacters",JSON.stringify(newFavouritesArray));
+          localStorage.setItem("favouritesCharacterIDs", JSON.stringify([...favouritesCharacterIDs]));
+          this.innerHTML = '<i class="fa-solid fa-heart fav-icon"></i> &nbsp; Add to Favourites';
+          document.querySelector(".remove-toast").setAttribute("data-visiblity","show");
+          setTimeout(function(){
+               document.querySelector(".remove-toast").setAttribute("data-visiblity","hide");
+          },1000);
+     }     
+}
+let themeButton = document.getElementById("theme-btn");
+themeButton.addEventListener("click",themeChanger);
+(function (){
+     let currentTheme = localStorage.getItem("theme");
+     if(currentTheme == null){
+          root.setAttribute("color-scheme","light");
+          themeButton.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+          themeButton.style.backgroundColor="#0D4C92";
+          localStorage.setItem("theme","light");
+          return;
+     }
+
+     switch(currentTheme){
+          case "light":
+               root.setAttribute("color-scheme","light");
+               themeButton.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+               themeButton.style.backgroundColor="#0D4C92";
+               break;
+          case "dark":
+               root.setAttribute("color-scheme","dark");
+               themeButton.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+               themeButton.style.backgroundColor="#FB2576";
+               themeButton.childNodes[0].style.color = "black";
+               break;
+     }
+})();
+function themeChanger(){
+     let root = document.getElementById("root");
+     if(root.getAttribute("color-scheme") == "light"){
+          root.setAttribute("color-scheme","dark");
+          themeButton.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+          themeButton.style.backgroundColor="#FB2576";
+          themeButton.childNodes[0].style.color = "black";
+          localStorage.setItem("theme","dark");
+     }
+     else if(root.getAttribute("color-scheme") == "dark"){
+          root.setAttribute("color-scheme","light");
+          themeButton.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+          themeButton.style.backgroundColor="#0D4C92";
+          themeButton.childNodes[0].style.color = "white";
+          localStorage.setItem("theme","light");
+     }
+}
---
+++ js/script.js
@@ -0,0 +1,183 @@
+let searchBar = document.getElementById("search-bar");
+let searchResults = document.getElementById("search-results");
+searchBar.addEventListener("input", () => searchHeros(searchBar.value));
+async function searchHeros(textSearched) {
+     if (textSearched.length == 0) {
+          searchResults.innerHTML = ``;
+          return;
+     }
+     await fetch(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${textSearched}&apikey=9ab871748d83ae2eb5527ffd69e034de&hash=d35377547e551cd64a60657d2517bb7f?ts=1`)
+          .then(res => res.json()) 
+          .then(data => showSearchedResults(data.data.results))
+}
+
+function showSearchedResults(searchedHero) {
+     let favouritesCharacterIDs = localStorage.getItem("favouritesCharacterIDs");
+     if(favouritesCharacterIDs == null){
+          favouritesCharacterIDs = new Map();
+     }
+     else if(favouritesCharacterIDs != null){
+          favouritesCharacterIDs = new Map(JSON.parse(localStorage.getItem("favouritesCharacterIDs")));
+     }
+     searchResults.innerHTML = ``;
+     let count = 1;
+     for (const key in searchedHero) {
+          if (count <= 5) {
+               let hero = searchedHero[key];
+               searchResults.innerHTML +=
+                    `
+               <li class="flex-row single-search-result">
+                    <div class="flex-row img-info">
+                         <img src="${hero.thumbnail.path+'/portrait_medium.' + hero.thumbnail.extension}" alt="">
+                         <div class="hero-info">
+                              <a class="character-info" href="./more-info.html">
+                                   <span class="hero-name">${hero.name}</span>
+                              </a>
+                         </div>
+                    </div>
+                    <div class="flex-col buttons">
+                         <!-- <button class="btn"><i class="fa-solid fa-circle-info"></i> &nbsp; More Info</button> -->
+                         <button class="btn add-to-fav-btn">${favouritesCharacterIDs.has(`${hero.id}`) ? "<i class=\"fa-solid fa-heart-circle-minus\"></i> &nbsp; Remove from Favourites" :"<i class=\"fa-solid fa-heart fav-icon\"></i> &nbsp; Add to Favourites</button>"}
+                    </div>
+                    <div style="display:none;">
+                         <span>${hero.name}</span>
+                         <span>${hero.description}</span>
+                         <span>${hero.comics.available}</span>
+                         <span>${hero.series.available}</span>
+                         <span>${hero.stories.available}</span>
+                         <span>${hero.thumbnail.path+'/portrait_uncanny.' + hero.thumbnail.extension}</span>
+                         <span>${hero.id}</span>
+                         <span>${hero.thumbnail.path+'/landscape_incredible.' + hero.thumbnail.extension}</span>
+                         <span>${hero.thumbnail.path+'/standard_fantastic.' + hero.thumbnail.extension}</span>
+                    </div>
+               </li>
+               `
+          }
+          count++;
+     }
+     events();
+}
+function events() {
+     let favouriteButton = document.querySelectorAll(".add-to-fav-btn");
+     favouriteButton.forEach((btn) => btn.addEventListener("click", addToFavourites));
+
+     let characterInfo = document.querySelectorAll(".character-info");
+     characterInfo.forEach((character) => character.addEventListener("click", addInfoInLocalStorage))
+}
+function addToFavourites() {
+
+     if (this.innerHTML == '<i class="fa-solid fa-heart fav-icon"></i> &nbsp; Add to Favourites') {
+          let heroInfo = {
+               name: this.parentElement.parentElement.children[2].children[0].innerHTML,
+               description: this.parentElement.parentElement.children[2].children[1].innerHTML,
+               comics: this.parentElement.parentElement.children[2].children[2].innerHTML,
+               series: this.parentElement.parentElement.children[2].children[3].innerHTML,
+               stories: this.parentElement.parentElement.children[2].children[4].innerHTML,
+               portraitImage: this.parentElement.parentElement.children[2].children[5].innerHTML,
+               id: this.parentElement.parentElement.children[2].children[6].innerHTML,
+               landscapeImage: this.parentElement.parentElement.children[2].children[7].innerHTML,
+               squareImage: this.parentElement.parentElement.children[2].children[8].innerHTML
+          }
+          let favouritesArray = localStorage.getItem("favouriteCharacters");          if (favouritesArray == null) {
+               favouritesArray = [];
+          } else {
+               favouritesArray = JSON.parse(localStorage.getItem("favouriteCharacters"));
+          }
+          let favouritesCharacterIDs = localStorage.getItem("favouritesCharacterIDs");
+          if (favouritesCharacterIDs == null) {
+               favouritesCharacterIDs = new Map();
+          } else {
+               favouritesCharacterIDs = new Map(JSON.parse(localStorage.getItem("favouritesCharacterIDs")));
+          }
+          favouritesCharacterIDs.set(heroInfo.id, true);
+ 
+          favouritesArray.push(heroInfo);
+
+          localStorage.setItem("favouritesCharacterIDs", JSON.stringify([...favouritesCharacterIDs]));
+          localStorage.setItem("favouriteCharacters", JSON.stringify(favouritesArray));
+          this.innerHTML = '<i class="fa-solid fa-heart-circle-minus"></i> &nbsp; Remove from Favourites';
+          document.querySelector(".fav-toast").setAttribute("data-visiblity","show");
+          setTimeout(function(){
+               document.querySelector(".fav-toast").setAttribute("data-visiblity","hide");
+          },1000);
+     }
+     else{
+          let idOfCharacterToBeRemoveFromFavourites = this.parentElement.parentElement.children[2].children[6].innerHTML;
+          let favouritesArray = JSON.parse(localStorage.getItem("favouriteCharacters"));
+          let favouritesCharacterIDs = new Map(JSON.parse(localStorage.getItem("favouritesCharacterIDs")));
+          let newFavouritesArray = [];
+          favouritesCharacterIDs.delete(`${idOfCharacterToBeRemoveFromFavourites}`);
+          favouritesArray.forEach((favourite) => {
+               if(idOfCharacterToBeRemoveFromFavourites != favourite.id){
+                    newFavouritesArray.push(favourite);
+               }
+          });
+          localStorage.setItem("favouriteCharacters",JSON.stringify(newFavouritesArray));
+          localStorage.setItem("favouritesCharacterIDs", JSON.stringify([...favouritesCharacterIDs]));
+          this.innerHTML = '<i class="fa-solid fa-heart fav-icon"></i> &nbsp; Add to Favourites';
+          document.querySelector(".remove-toast").setAttribute("data-visiblity","show");
+          setTimeout(function(){
+               document.querySelector(".remove-toast").setAttribute("data-visiblity","hide");
+          },1000);
+
+     }     
+}
+
+function addInfoInLocalStorage() {
+     let heroInfo = {
+          name: this.parentElement.parentElement.parentElement.children[2].children[0].innerHTML,
+          description: this.parentElement.parentElement.parentElement.children[2].children[1].innerHTML,
+          comics: this.parentElement.parentElement.parentElement.children[2].children[2].innerHTML,
+          series: this.parentElement.parentElement.parentElement.children[2].children[3].innerHTML,
+          stories: this.parentElement.parentElement.parentElement.children[2].children[4].innerHTML,
+          portraitImage: this.parentElement.parentElement.parentElement.children[2].children[5].innerHTML,
+          id: this.parentElement.parentElement.parentElement.children[2].children[6].innerHTML,
+          landscapeImage: this.parentElement.parentElement.parentElement.children[2].children[7].innerHTML,
+          squareImage: this.parentElement.parentElement.parentElement.children[2].children[8].innerHTML
+     }
+
+     localStorage.setItem("heroInfo", JSON.stringify(heroInfo));
+}
+let themeButton = document.getElementById("theme-btn");
+themeButton.addEventListener("click",themeChanger);
+(function (){
+     let currentTheme = localStorage.getItem("theme");
+     if(currentTheme == null){
+          root.setAttribute("color-scheme","light");
+          themeButton.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+          themeButton.style.backgroundColor="#0D4C92";
+          localStorage.setItem("theme","light");
+          return;
+     }
+
+     switch(currentTheme){
+          case "light":
+               root.setAttribute("color-scheme","light");
+               themeButton.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+               themeButton.style.backgroundColor="#0D4C92";
+               break;
+          case "dark":
+               root.setAttribute("color-scheme","dark");
+               themeButton.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+               themeButton.style.backgroundColor="#FB2576";
+               themeButton.childNodes[0].style.color = "black";
+               break;
+     }
+})();
+function themeChanger(){
+     let root = document.getElementById("root");
+     if(root.getAttribute("color-scheme") == "light"){
+          root.setAttribute("color-scheme","dark");
+          themeButton.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+          themeButton.style.backgroundColor="#FB2576";
+          themeButton.childNodes[0].style.color = "black";
+          localStorage.setItem("theme","dark");
+     }
+     else if(root.getAttribute("color-scheme") == "dark"){
+          root.setAttribute("color-scheme","light");
+          themeButton.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+          themeButton.style.backgroundColor="#0D4C92";
+          themeButton.childNodes[0].style.color = "white";
+          localStorage.setItem("theme","light");
+     }
+}
